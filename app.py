3acfad98693b3e722d5bab10a24c8a58b838b607
yifanLin <51112522+yifanLin@users.noreply.github.com>
2021-04-04 22:34:57 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,10 +10,14 @@ import dash
 import dash_bio as dashbio
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_table
 import pandas as pd
 import plotly.express as px
 from dash.dependencies import Input, Output
 import numpy
+import base64
+import datetime
+import io
 
 # define const: external stylesheet, groups, time points, colors, and string const
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -27,6 +31,14 @@ clustergram_colors = ['#06081c', '#f49f76', '#e22e39', '#6a1e56', '#ffffff']
 condition = 'OSvsPS'
 p = '_Padjusted'
 l = '_logFC'
+
+# init variables
+df = pd.read_csv('deseq2_all_genes.csv')
+df_index = df.set_index('GeneName') # keep another indexed df for clustergram row names
+columns = list(df_index.columns.values)
+LFC = df_index[[i for i in columns if 'logFC' in i]]
+df_pca = df[df['GeneName'].isin(['KLF4', 'KLF2', 'HR', 'NPR1', 'FGFR3', 'CXCR4'])] #p0.1; lfc1
+tp_num_default = [1, 9, 24]
 
 # func: DE_filter
 # @param df: dataframe
@@ -47,15 +59,50 @@ def DE_filter(df, lfc, padj, tpList):
     del df['index']
     return df
 
-df = pd.read_csv('deseq2_all_genes.csv', sep=',')
-df_index = df.set_index('GeneName') # keep another indexed df for clustergram row names
-columns = list(df_index.columns.values)
-LFC = df_index[[i for i in columns if 'logFC' in i]]
-df_pca = df[df['GeneName'].isin(['KLF4', 'KLF2', 'HR', 'NPR1', 'FGFR3', 'CXCR4'])] #p0.1; lfc1
-tp_num_default = [1, 9, 24]
-# print(df_pca)
+# func: parse_contents
+# @param contents: contents of upload (type and string)
+# @param filename: name of file to parse
+# @param date: last update date of file
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
 
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            df_index = df.set_index('GeneName') # keep another indexed df for clustergram row names
+            columns = list(df_index.columns.values)
+            LFC = df_index[[i for i in columns if 'logFC' in i]]
+            df_pca = df[df['GeneName'].isin(['KLF4', 'KLF2', 'HR', 'NPR1', 'FGFR3', 'CXCR4'])] #p0.1; lfc1
+            tp_num_default = [1, 9, 24]
+            # partial display as there are too many rows
+            df = df[0:25]
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+            
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
 
+    return html.Div([
+        html.I(filename),
+        html.I(' preview'),
+        html.Br(),
+        html.I(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+    ])
+
+# app major layout style and components
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '70%','padding-left':'15%', 'padding-right':'15%'}, children=[ html.Br(),
     html.H1(
@@ -64,7 +111,32 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
             'textAlign': 'center',
             'color': colors['text'],
             'fontSize': 45}),
-    html.I('    ver 1.0.0, 04/02/2021'), html.Br(), html.Br(),
+    html.I('ver 1.0.0, 04/02/2021'), html.Br(), 
+    html.I('ver 2.0.0, 04/04/2021'), html.Br(), html.Br(),
+#========================pt0: upload a csv file to get started==========================#
+    html.H1(
+        children = '0 - Please upload your time-series RNA-seq dataset to get started',
+        style={
+            'textAlign': 'center',
+            'color': colors['text'],
+            'fontSize': 35}),
+    dcc.Upload(
+        id='upload-file',
+        children=html.Div([html.A('Upload a File in .csv Format')]),
+        filename='deseq2_all_genes.csv',
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'},
+        # Allow multiple files to be uploaded
+        multiple=True),
+    html.Div(id='output-file-upload'),
+
 #===========pt1: volcano plot to explore genes of interest based on custom LFC==========#
     html.H1(
         children = '1 - Volcanoplots of Transcriptional Levels at Inidividual Timepoint',
@@ -77,7 +149,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
    dcc.Dropdown(
         id='volcano-tp-input',
         options=[
-            {'label': row, 'value': row} for row in tp]), html.Br(),
+            {'label': row, 'value': row} for row in tp],
+        value='1hr'), html.Br(),
     
     html.I('Use the slider to customize Log2 Fold Change range: '),
     # slilder to customize LFC TODO: remove outlier and display
@@ -89,8 +162,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
         marks={
             i: {'label': str(i)} for i in range(-3, 3)
         },
-        value=[-1.3, 1.3]), html.Br(),
-    html.I('Note: significant p-adj cutoff == 0.05 in this section. '),
+        value=[-1.3, 1.3]), 
+    html.I('Note: significant p-adj cutoff == 0.05 in this section. '), html.Hr(),
 
     # volcano plot div
     dcc.Graph(
@@ -109,9 +182,9 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
             col='#111111',
             title="Volcano plot of Human Umbilical Vein Cell RNAseq at 1hr",
             xlabel="log2 Fold Change",
-            ylabel="-log10(p_adjusted)")), html.Br(),
+            ylabel="-log10(p_adjusted)")), html.Hr(),html.Br(),
 
-#=====================pt2: heatmap overview of transcriptional levels====================#
+#=====================pt2: heatmap overview of transcriptional levels===================#
     html.H1(
         children = '2 - Transcriptional Levels Examination with Clustergarm',
         style={
@@ -182,8 +255,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
                 {'label': '24hr', 'value': '24hr'}],
             value=[],
             labelStyle={'display': 'inline-block'}),
-        style={'display': 'inline-block'}),
-
+        style={'display': 'inline-block'}), 
     # clustergram div
     dcc.Graph(
         id='my-clustergram',
@@ -197,9 +269,9 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
             height=1200,
             width=900,
             color_map= [[0.0, '#06081c'],[0.25, '#f49f76'],[0.5, '#e22e39'],[0.75, '#6a1e56'],[1, '#ffffff']])), 
-    html.Br(),
+    html.Hr(), html.Br(),
 
-#=====================pt3: PCA analysis based on top ranked DE genes=====================#
+#=====================pt3: PCA analysis based on top ranked DE genes====================#
     html.H1(
         children = '3 - Customized 3D Principle Component Analysis',
         style={
@@ -232,8 +304,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
             options=[
                 {'label': i, 'value': i} for i in list(df_index.index)],
             value=['KLF4', 'KLF2', 'HR', 'NPR1', 'FGFR3', 'CXCR4'],
-            multi=True),
-    
+            multi=True), 
     # 3D pca div
     dcc.Graph(
         id='pca-plot',
@@ -248,8 +319,24 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'width': '
                 'OSvsPS'+str(tp_num_default[2])+'_logFC':'24hr log2FC'},
             color='GeneName',
             width=800,
-            height=600))
+            height=600)), html.Hr()
 ])
+
+# file upload callback and update function
+# @param contents: upload info including type and string
+# @param filenames: filename
+# @param lastMods: lastly modified time
+@app.callback(
+    dash.dependencies.Output('output-file-upload', 'children'),
+    dash.dependencies.Input('upload-file', 'contents'),
+    dash.dependencies.State('upload-file', 'filename'),
+    dash.dependencies.State('upload-file', 'last_modified'))
+def upload_file(contents, filenames, lastMods):
+    if contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(contents, filenames, lastMods)]
+        return children
 
 # volcano plot callback and update function
 # @param: 'volcano-tp-input' tp
